c8b2688a7a37729e9b086b7a13a2a42846d4586c
Glenn0805 <cavitaglenn22131@gmail.com>
2023-11-08 12:52:08 +0800

    added board header

--- src/board/BoardContainer.tsx
+++ src/board/BoardContainer.tsx
@@ -1,31 +1,33 @@
-import { DndContext, DragOverlay, KeyboardSensor, closestCorners, useSensor, useSensors } from '@dnd-kit/core'
-import { Board, CardType} from 'Shared/type/KanbanType'
+import { DndContext, DragOverlay, KeyboardSensor, UniqueIdentifier, closestCorners, useSensor, useSensors } from '@dnd-kit/core'
+import { Board, CardType } from 'Shared/type/KanbanType'
 import ListComponent from './components/ListComponent'
 import useBoardStore from './board-store'
-import {addCardToList, handleDragEnd, handleDragOver, handleDragStart, toggleAddEditModal, updateCardToList } from './board-action'
+import { addCardToList, handleDragEnd, handleDragOver, handleDragStart, toggleAddEditModal, updateCardToList } from './board-action'
 import { sortableKeyboardCoordinates } from '@dnd-kit/sortable'
 import ActiveCardComponent from './components/ActiveCardComponent'
 import AddEditCardModal from './components/AddEditCardModal'
-import { MouseSensor,PointerSensor } from './config/CustomSensor'
+import { MouseSensor, PointerSensor } from './config/CustomSensor'
 import { ToggleAddEditModal } from './type/BoardType'
+import { Button, Card, Typography } from 'antd'
 
 
 const BoardContainer = (props: Board) => {
     const { boardId, boardName, lists } = props
     const boardStore = useBoardStore()
-    const { dispatch, data, activeCard ,addEditModal} = boardStore
+    const { dispatch, data, activeCard, addEditModal } = boardStore
 
-    const toggleModal : ToggleAddEditModal = ({actionType,listName,listId,card})=>{
+    const toggleModal: ToggleAddEditModal = ({ actionType, listName, listId, card }) => {
         dispatch(toggleAddEditModal({
-                isAddEditModalOpen:addEditModal.modal.isAddEditModalOpen,
-                listName:listName,
-                listId:listId,
-                actionType:actionType},
-                card))
+            isAddEditModalOpen: addEditModal.modal.isAddEditModalOpen,
+            listName: listName,
+            listId: listId,
+            actionType: actionType
+        },
+            card))
     }
 
     const renderLists = lists?.map((list) => (
-        <ListComponent key={list.listId} list={list} openModal={toggleModal}/>
+        <ListComponent key={list.listId} list={list} openModal={toggleModal} />
     ))
 
     const sensors = useSensors(
@@ -36,18 +38,23 @@ const BoardContainer = (props: Board) => {
         })
     );
 
-    const handleAddCard = (card:CardType,listId:string)=>{
-        dispatch(addCardToList(card,data,boardId,listId))
+    const handleAddCard = (card: CardType, listId: string) => {
+        dispatch(addCardToList(card, data, boardId, listId))
     }
 
-    const handleUpdateCard = (card:CardType,listId:string,cardId:string)=>{
-        dispatch(updateCardToList(card,data,boardId,listId,cardId))
+    const handleUpdateCard = (card: CardType, listId: string, cardId: UniqueIdentifier) => {
+        dispatch(updateCardToList(card, data, boardId, listId, cardId))
     }
     return (
         <>
-            <div className='flex-col'>
-                <h1 className='text-yellow-300'>{boardName}</h1>
-                <div className='p-10 flex gap-9'>
+            <div className='flex-col p-4'>
+                <Card className=' mb-4'>
+                    <div className='flex gap-1'>
+                        <Typography.Text className='text-2xl text-center font-semibold'>{boardName}</Typography.Text>
+                        <Button type='text'>Create List</Button>
+                    </div>
+                </Card>
+                <div className=' flex gap-4'>
                     <DndContext
                         sensors={sensors}
                         collisionDetection={closestCorners}
@@ -55,8 +62,8 @@ const BoardContainer = (props: Board) => {
                         onDragStart={(event) => {
                             dispatch(handleDragStart(event, lists))
                         }}
-                        onDragOver={(event)=>{
-                            dispatch(handleDragOver(event,lists,boardId,data))
+                        onDragOver={(event) => {
+                            dispatch(handleDragOver(event, lists, boardId, data))
                         }}
                         onDragEnd={(event) => {
                             dispatch(handleDragEnd(event, lists, boardId, data))
@@ -66,7 +73,7 @@ const BoardContainer = (props: Board) => {
                     </DndContext>
                 </div>
             </div>
-            <AddEditCardModal addCard={handleAddCard} updateCard={handleUpdateCard} modal={addEditModal.modal} card={addEditModal.card} key={"addEditCardModal"} onClose={toggleModal}/>
+            <AddEditCardModal addCard={handleAddCard} updateCard={handleUpdateCard} modal={addEditModal.modal} card={addEditModal.card} key={"addEditCardModal"} onClose={toggleModal} />
 
         </>
     )
